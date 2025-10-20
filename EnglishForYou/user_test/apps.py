b3288b7dfdad09a65216ae19861e36ebb946bf0b
Andrey <andrey.n550@gmail.com>
2025-10-20 11:28:25 +0300

    fix2

--- EnglishForYou/user_test/apps.py
+++ EnglishForYou/user_test/apps.py
@@ -1,6 +1,20 @@
+"""
+Конфигурация приложения user_test.
+
+Этот файл содержит настройки Django-приложения для тестирования уровня английского языка.
+Используется Django для автоматической регистрации приложения в проекте.
+"""
+
 from django.apps import AppConfig
 
 
 class UserTestConfig(AppConfig):
+    """
+    Класс конфигурации приложения тестирования.
+    
+    Параметры:
+        default_auto_field: Тип поля для автоматических первичных ключей (BigAutoField)
+        name: Название приложения в Django проекте
+    """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'user_test'
